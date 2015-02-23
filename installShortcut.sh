3ad5f40993f5db01fe9596ee809687f59d146caf
James Milne <s4b3r6@gmail.com>
2015-02-23 02:55:59 +0000

    Problem with exporting variables present.

--- installShortcut.sh
+++ installShortcut.sh
@@ -1,17 +1,45 @@
-#!/bin/env bash
+#!/bin/bash
+
+
+function defineVariables()
+{
+	# Take user input and make a variable.
+	SHORTCUT=shortcuts/"$1"
+	echo "$1"
+	echo "$SHORTCUT"
+}
 
 function checkNewlineChars()
 {
-	# Append a newline character if one doesn't exist yet.
+	# Append a newline character
+	{ cat ~/.bashrc; echo ""; } > ~/.bashrc.new
+	cp ~/.bashrc ~/.bashrc.bak
+	echo "Backing up ~/.bashrc to ~/bashrc.bak" 
+	mv ~/.bashrc.new ~/.bashrc
+	echo "Replaced ~/.bashrc with new version."
 }
 
 function checkShortcutAppended()
 {
 	# Check if shortcut already exists in ~/.bashrc
+	if grep -q "$Shorcut" ~/.bashrc; then
+		echo "Shortcut already exists. Exiting."
+		Continue=false
+	else
+		echo "Shortcut not found in ~/.bashrc. Proceeding."
+		Continue=true
+	fi
 }
 
 function append()
 {
 	# Only run after checkShortcutAppended, adds the shortcut file to ~/.bashrc
-	cat "$1" >> ~/.bashrc
+	if [ $Continue = true ] ; then
+		cat "$Shortcut" >> ~/.bashrc
+	fi
 }
+
+defineVariables
+checkShortcutAppended
+checkNewlineChars
+append
